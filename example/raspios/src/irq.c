ba6171ef2dead0902bb585287c4c6fc3d756598e
Sho SHIMOMURA <graueule@gmail.com>
2026-01-16 00:13:00 +0900

    use virtualized generic timer in raspios

--- example/raspios/src/irq.c
+++ example/raspios/src/irq.c
@@ -31,7 +31,7 @@ const char *entry_error_messages[] = {
 
 void enable_interrupt_controller()
 {
-	put32(ENABLE_IRQS_1, SYSTEM_TIMER_IRQ_1);
+	// put32(ENABLE_IRQS_1, SYSTEM_TIMER_IRQ_1);
 }
 
 void show_invalid_entry_message(int type, unsigned long esr, unsigned long address)
@@ -41,6 +41,17 @@ void show_invalid_entry_message(int type, unsigned long esr, unsigned long addre
 
 void handle_irq(void)
 {
+	unsigned long cntv_ctl;
+	asm volatile("mrs %0, cntv_ctl_el0" : "=r"(cntv_ctl));
+	// CNTP_CTL_EL0: Counter-timer Physical Timer Control register
+	// [2] ISTATUS: The status of the timer.
+	// [1] IMASK: Timer interrupt mask bit.
+	// [0] ENABLE: Enables the timer.
+	if (cntv_ctl & 0x4) {
+		handle_timer_irq();
+		return;
+	}
+
 	unsigned int irq = get32(IRQ_PENDING_1);
 	switch (irq) {
 		case (SYSTEM_TIMER_IRQ_1):
--- example/raspios/src/timer.c
+++ example/raspios/src/timer.c
@@ -1,23 +1,24 @@
 #include "utils.h"
 #include "printf.h"
 #include "sched.h"
-#include "peripherals/timer.h"
 
-const unsigned int interval = 20000;
-unsigned int curVal = 0;
+const unsigned int interval = 20000; // 20ms
 
 void timer_init ( void )
 {
-	curVal = get32(TIMER_CLO);
-	curVal += interval;
-	put32(TIMER_C1, curVal);
+	unsigned long frequency;
+	asm volatile("mrs %0, cntfrq_el0" : "=r"(frequency));
+	unsigned long ticks = (frequency * interval) / 1000000;
+	asm volatile("msr cntv_tval_el0, %0" : : "r"(ticks));
+	// enable virtual timer interrupt
+	asm volatile("msr cntv_ctl_el0, %0" : : "r"(1));
 }
 
 void handle_timer_irq( void ) 
 {
-	curVal += interval;
-	put32(TIMER_C1, curVal);
-	put32(TIMER_CS, TIMER_CS_M1);
-// printf("TICK %d\n", get_cpuid());
+	unsigned long frequency;
+	asm volatile("mrs %0, cntfrq_el0" : "=r"(frequency));
+	unsigned long ticks = (frequency * interval) / 1000000;
+	asm volatile("msr cntv_tval_el0, %0" : : "r"(ticks));
 	timer_tick();
 }
