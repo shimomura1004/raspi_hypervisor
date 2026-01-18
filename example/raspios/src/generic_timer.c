492334ffb88e23dda9015325480f34c511da9e8f
Sho SHIMOMURA <graueule@gmail.com>
2026-01-18 13:46:52 +0900

    bugfix: clear HCR_EL2.VI after handling interrupt in VM

--- example/raspios/src/generic_timer.c
+++ example/raspios/src/generic_timer.c
@@ -1,5 +1,6 @@
 #include "utils.h"
 #include "sched.h"
+#include "peripherals/irq.h"
 #include "debug.h"
 
 static const unsigned int interval_ms = 200;
@@ -21,5 +22,12 @@ void handle_timer_irq( void )
 	asm volatile("mrs %0, cntfrq_el0" : "=r"(frequency));
 	unsigned long ticks = (frequency * interval_ms) / 1000;
 	asm volatile("msr cntv_tval_el0, %0" : : "r"(ticks));
+
+	// ハイパーバイザ環境では、cntv_tval_el0 への書き込みをトラップすることができないので、
+	// ゲストが仮想タイマ割込みを処理したタイミングを自分で知ることができない
+	// よって明示的に IRQ_PENDING_1 にアクセスすることで MMIO によるトラップを発生させ、
+	// ハイパーバイザに仮想割り込みの状態を更新(クリア)させる
+	get32(IRQ_PENDING_1);
+
 	timer_tick();
 }
