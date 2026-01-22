21a07e8a37c33b9c3a05f7c907dd206cb4af1355
Sho SHIMOMURA <graueule@gmail.com>
2026-01-23 00:31:11 +0900

    add defs

--- include/peripherals/mini_uart.h
+++ include/peripherals/mini_uart.h
@@ -19,4 +19,82 @@
 #define AUX_MU_STAT_REG (PBASE+0x00215064)
 #define AUX_MU_BAUD_REG (PBASE+0x00215068)
 
+// AUX_MU_IO_REG
+//   [31:8] Reserved
+//   [7:0]  Data
+//     DLAB=1, RW: Access the LS bits of the 16-bit baudrate register
+//     DLAB=0, W: Data written is put in the transmit FIFO
+//     DLAB=0, R: Data read is taken from the receive FIFO
+#define AUX_MU_IO_REG_DATA  (0xff)
+
+// AUX_MU_IER_REG: Interrupt Enable Register
+//   [31:8] Reserved
+//   DLAB=1
+//   [7:0]  Enable Transmit FIFO interrupt
+//   DLAB=0
+//   [7:4]  Ignored
+//   [3:2]  must be set to 1 to receive interrupts
+//   [1]    Enable transmit interrupts
+//   [0]    Enable receive interrupts
+#define AUX_MU_IER_REG_ENABLE_TRANSMIT_INTERRUPTS   (0b1 << 1)
+#define AUX_MU_IER_REG_ENABLE_RECEIVE_INTERRUPTS    (0b1 << 0)
+
+// AUX_MU_IIR_REG: Interrupt Identity Register
+//   [31:8] Reserved
+//   [7:6]  FIFO enables
+//   [5:4]  Always read as zero
+//   [3]    Always read as zero as the mini UART has no timeout function
+//   [2:1]  READ: Interrupt ID bits
+//                00: No interrupts
+//                01: Transmit holding register empty
+//                10: Receiver holds valid byte
+//                11: Not possible
+//          WRITE: FIFO clear bits
+//                Writing with bit 1 set will clear the receive FIFO
+//                Writing with bit 2 set will clear the transmit FIFO
+//   [0]    Interrupt pending
+//          This bit is clear whenever an interrupt is pending
+#define AUX_MU_IIR_REG_INTERRUPT_ID         (0b11 << 1)
+#define AUX_MU_IIR_REG_INTERRUPT_PENDING    (0b01 << 0)
+
+// AUX_MU_LCR_REG: Line Control Register?
+//   [31:8] Reserved
+//   [7]    DLAB asscess
+//          If set the first to Mini UART register give access
+//          the Bauderate register. During operation this bit must be cleared.
+//   [6]    Break
+//          If set high the UART1_TX line is pulled low continuously.
+//          If held for at least 12 bits times that will indicate a break condition.
+//   [5:2]  Reserved
+//   [1:0]  Data size
+//            00: the UART works in 7-bit mode
+//            11: the UART works in 8-bit mode
+
+// AUX_MU_LSR_REG: Line Status Register?
+//   [31:8] Reserved
+//   [7]    Reserved
+//   [6]    Transmitter idle
+//          This bit is set if the transmit FIFO is empty and the transmitter is idle.
+//   [5]    Transmitter empty
+//          This bit is set if the transmit FIFO can accept at least one byte.
+//   [4:2]  Reserved
+//   [1]    Receiver Overrun
+//          This bit is set if there was a receiver overrun.
+//          - one or more characters arrived whilst the receive FIFO was full.
+//          - The newly arrived character was discarded.
+//          This bit is cleared each time this register is read.
+//   [0]    Data ready
+//          This bit is set if the receive FIFO holds at least 1 symbol
+
+// AUX_MU_MSR_REG: Modem Status Register?
+//   [31:8] Reserved
+//   [7:6]  Reserved
+//   [5]    CTS status (CTS: clear to send)
+//          This bit is the inverse of the UART1_CTS input Thus:
+//            If set the UART1_CTS pin is low
+//            If clear the UART1_CTS pin is high
+//          CTS と RTS はクロスして通信相手とつなぎ、CTS が low のときに送信可能という意味
+//   [4]?
+//   [3:0]  reserved
+
 #endif  /*_P_MINI_UART_H */
