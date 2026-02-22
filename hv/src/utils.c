2717eb351888ba03dbbe5931fba4a13601c93072
Sho SHIMOMURA <graueule@gmail.com>
2026-02-22 17:23:11 +0900

    fix warning

--- hv/src/utils.c
+++ hv/src/utils.c
@@ -8,7 +8,7 @@ int abs(int n) {
 
 char *strncpy(char *dst, const char *src, size_t n) {
     size_t i;
-    for (int i = 0; i < n && src[i] != '\0'; i++) {
+    for (i = 0; i < n && src[i] != '\0'; i++) {
         dst[i] = src[i];
     }
     for (; i < n; i++) {
