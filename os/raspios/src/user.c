b5621e54b1c78cc61c220a3078eebe8964d0911e
Sho SHIMOMURA <graueule@gmail.com>
2026-02-28 00:51:03 +0900

    minor fix

--- os/raspios/src/user.c
+++ os/raspios/src/user.c
@@ -2,11 +2,11 @@
 #include "user.h"
 #include "printf.h"
 
-void loop(char* str)
+void loop(char* str, int length)
 {
 	char buf[2] = {""};
 	while (1){
-		for (int i = 0; i < 5; i++){
+		for (int i = 0; i < length; i++){
 			buf[0] = str[i];
 			call_sys_write(buf);
 			// todo: ビジーループをやめて sleep を使いたい
@@ -27,7 +27,7 @@ void user_process()
 	if (pid == 0){
 		call_sys_set_priority(3);
 		call_sys_write("Child process started 0\n\r");
-		loop("abcde");
+		loop("*", 1);
 	} else {
 		int pid = call_sys_fork();
 		if (pid < 0) {
@@ -38,12 +38,12 @@ void user_process()
 		if (pid == 0) {
 			call_sys_set_priority(2);
 			call_sys_write("Child process started 1\n\r");
-			loop("12345");
+			loop("+", 1);
 		}
 		else {
 			call_sys_set_priority(1);
 			call_sys_write("Child process started 2\n\r");
-			loop("ABCDE");
+			loop("-", 1);
 		}
 	}
 }
