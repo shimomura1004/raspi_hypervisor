ace324a38574828192265036ecd6f0d7837fee87
SHIMOMURA Sho <graueule@gmail.com>
2026-03-07 10:17:53 +0900

    add handler skelton

--- sm/src/main.c
+++ sm/src/main.c
@@ -1,9 +1,20 @@
 #include "log.h"
+#include "smccc.h"
 
 void print_boot_message() {
     sm_print("Secure monitor initialized\r\n");
 }
 
 void handle_smc(unsigned long x0, unsigned long x1, unsigned long x2, unsigned long x3, unsigned long x4) {
-  sm_print("SMC received\r\n");
+    switch (x0) {
+        case PSCI_0_2_FN64_CPU_ON:
+            // todo: 出力が改行されない
+            sm_print("SMC: PSCI_CPU_ON\r\n");
+            // todo: 実装する
+            break;
+        default:
+            // todo: sm_print で printf フォーマットに対応する
+            sm_print("SMC: Unknown Function ID: ");
+            break;
+    }
 }
