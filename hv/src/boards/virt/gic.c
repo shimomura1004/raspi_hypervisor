8e40c556c60e8c939caad1f25f36a1c590b3dc97
Sho SHIMOMURA <graueule@gmail.com>
2026-03-13 01:10:28 +0900

    add gic for virt board

---
+++ hv/src/boards/virt/gic.c
@@ -0,0 +1,42 @@
+#include "utils.h"
+#include "board_config.h"
+#include <inttypes.h>
+
+/* GICv2 Registers (relative to GIC_DIST_BASE and GIC_CPU_BASE) */
+#define GICD_CTLR       (GIC_DIST_BASE + 0x000)
+#define GICD_TYPER      (GIC_DIST_BASE + 0x004)
+#define GICD_ISENABLER  (GIC_DIST_BASE + 0x100)
+#define GICD_ICENABLER  (GIC_DIST_BASE + 0x180)
+#define GICD_IPRIORITYR (GIC_DIST_BASE + 0x400)
+#define GICD_ITARGETSR  (GIC_DIST_BASE + 0x800)
+#define GICD_ICFG       (GIC_DIST_BASE + 0xc00)
+
+#define GICC_CTLR       (GIC_CPU_BASE + 0x000)
+#define GICC_PMR        (GIC_CPU_BASE + 0x004)
+#define GICC_IAR        (GIC_CPU_BASE + 0x00c)
+#define GICC_EOIR       (GIC_CPU_BASE + 0x010)
+
+void enable_local_interrupt_controller(unsigned long cpuid) {
+    /* Enable GIC CPU interface */
+    put32(GICC_PMR, 0xff);    /* Allow all priority levels */
+    put32(GICC_CTLR, 0x1);    /* Enable Group 0 (or Group 1 if GICv2) */
+}
+
+void enable_legacy_interrupt_controller(void) {
+    /* Global GIC Distributor initialization */
+    put32(GICD_CTLR, 0x1);    /* Enable Distributor */
+}
+
+/* Note: handle_irq will be called from vectors. 
+ * For GIC, we need to read IAR to get the IRQ number and write EOIR when done.
+ */
+void handle_irq(void) {
+    unsigned int iar = get32(GICC_IAR);
+    unsigned int irq = iar & 0x3ff;
+
+    if (irq < 1022) {
+        /* Handle specific IRQs here (e.g., UART, Timer) */
+        /* For now, just clear it */
+        put32(GICC_EOIR, iar);
+    }
+}
