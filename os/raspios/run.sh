bb3ecda6f83d33d73ff117a9aa63a16f37fbca1d
SHIMOMURA Sho <graueule@gmail.com>
2026-03-14 17:15:21 +0900

    bugfix: set CORE0 PC to proper address

--- os/raspios/run.sh
+++ os/raspios/run.sh
@@ -9,7 +9,7 @@ KERNEL_IMG=./build/kernel8.img
 
 echo "Run OS on $BOARD"
 if [ "$BOARD" = "virt" ]; then
-    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2 -cpu cortex-a53 -device loader,file=$KERNEL_IMG,addr=0x40100000 -nographic -serial mon:stdio -net none -s $*
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2 -cpu cortex-a53 -smp 4 -device loader,file=$KERNEL_IMG,addr=0x40100000,cpu-num=0 -nographic -serial mon:stdio -net none -s $*
 else
     DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=$KERNEL_IMG,addr=0x0 -nographic -serial null -serial mon:stdio -s $*
 fi
