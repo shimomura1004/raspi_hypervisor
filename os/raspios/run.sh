9be4f36bb875dbb4bf9ffe0ad4ec558f77e98668
SHIMOMURA Sho <graueule@gmail.com>
2026-03-14 10:57:32 +0900

    update Makefile to support qemu-virt

--- os/raspios/run.sh
+++ os/raspios/run.sh
@@ -4,4 +4,10 @@
 # イメージをロードするアドレスを指定できないのでフラットバイナリが使えない
 #DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel kernel8.img -nographic -serial null -serial mon:stdio -s $*
 
-DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=./build/kernel8.img,addr=0x0 -nographic -serial null -serial mon:stdio -s $*
+BOARD=${BOARD:-raspi3}
+
+if [ "$BOARD" = "virt" ]; then
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2 -cpu cortex-a53 -device loader,file=./build/kernel8.img,addr=0x40000000 -nographic -serial mon:stdio -s $*
+else
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=./build/kernel8.img,addr=0x0 -nographic -serial null -serial mon:stdio -s $*
+fi
