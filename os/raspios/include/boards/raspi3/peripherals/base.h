79b7756e3de29e08834a361a5df277773c457c7f
Sho SHIMOMURA <graueule@gmail.com>
2026-03-20 14:15:26 +0900

    add notes

--- os/raspios/include/boards/raspi3/peripherals/base.h
+++ os/raspios/include/boards/raspi3/peripherals/base.h
@@ -3,8 +3,18 @@
 
 #include "mm.h"
 
-#define PHYS_MEMORY_BASE	0x00000000
-#define DEVICE_BASE 		0x3F000000	
-#define PBASE 			(VA_START + DEVICE_BASE)
+// BCM2837 ではペリフェラルが2つのブロックに分かれて配置されている
+//   0x3F000000 - 0x3FFFFFFF : General Purpose I/O
+//   0x40000000 - 0x401FFFFF : Local Peripherals
+
+#define PHYS_MEMORY_BASE        0x00000000
+#define RAM_SIZE                0x3F000000
+#define DEVICE_BASE             0x3F000000	
+// Peripheral base address
+#define PBASE                   (VA_START + DEVICE_BASE)
+
+// BCM2837 のときのみ必要になる Local Peripherals 用の定数定義
+#define LOCAL_PERIPHERAL_BASE   0x40000000
+#define LOCAL_PERIPHERAL_SIZE   0x00200000
 
 #endif  /*_P_BASE_H */
--- os/raspios/include/boards/virt/peripherals/base.h
+++ os/raspios/include/boards/virt/peripherals/base.h
@@ -3,10 +3,11 @@
 
 #include "mm.h"
 
-#define PHYS_MEMORY_BASE	0x40000000
+#define PHYS_MEMORY_BASE    0x40000000
+#define RAM_SIZE            0x40000000
 
 // Virt GIC Base
-#define DEVICE_BASE 		0x08000000	
-#define PBASE 			(VA_START + DEVICE_BASE)
+#define DEVICE_BASE         0x08000000	
+#define PBASE               (VA_START + DEVICE_BASE)
 
 #endif  /*_P_BASE_H */
