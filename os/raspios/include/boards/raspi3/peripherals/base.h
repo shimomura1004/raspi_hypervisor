e605bf5d19c4220af019c5fee639b9781d779482
Sho SHIMOMURA <graueule@gmail.com>
2026-03-21 13:48:04 +0900

    refactor: fix constant names

--- os/raspios/include/boards/raspi3/peripherals/base.h
+++ os/raspios/include/boards/raspi3/peripherals/base.h
@@ -7,14 +7,16 @@
 //   0x3F000000 - 0x3FFFFFFF : General Purpose I/O
 //   0x40000000 - 0x401FFFFF : Local Peripherals
 
-#define PHYS_MEMORY_BASE        0x00000000
+#define RAM_BASE                0x00000000
 #define RAM_SIZE                0x3F000000
 #define DEVICE_BASE             0x3F000000	
-// Peripheral base address
-#define PBASE                   (VA_START + DEVICE_BASE)
+#define DEVICE_SIZE             0x01000000
 
 // BCM2837 のときのみ必要になる Local Peripherals 用の定数定義
 #define LOCAL_PERIPHERAL_BASE   0x40000000
 #define LOCAL_PERIPHERAL_SIZE   0x00200000
+
+// Peripheral base address
+#define PBASE                   (VA_START + DEVICE_BASE)
 
 #endif  /*_P_BASE_H */
--- os/raspios/include/boards/virt/peripherals/base.h
+++ os/raspios/include/boards/virt/peripherals/base.h
@@ -3,11 +3,14 @@
 
 #include "mm.h"
 
-#define PHYS_MEMORY_BASE    0x40000000
+#define RAM_BASE            0x40000000
 #define RAM_SIZE            0x40000000
 
-// Virt GIC Base
+// virt ボードではペリフェラルはひとつのブロックに配置されている
 #define DEVICE_BASE         0x08000000	
+#define DEVICE_SIZE         0x02000000
+
+// Peripheral base address
 #define PBASE               (VA_START + DEVICE_BASE)
 
 #endif  /*_P_BASE_H */
