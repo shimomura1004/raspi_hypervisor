f6c35639f9b4e345d2d39eff9879422ac7dac7cb
Sho SHIMOMURA <graueule@gmail.com>
2026-03-29 21:20:06 +0900

    modify linker script and runner script to run OS in EL1

--- os/raspios/run.sh
+++ os/raspios/run.sh
@@ -6,10 +6,11 @@
 
 BOARD=${BOARD:-raspi3}
 KERNEL_IMG=./build/kernel8.img
+KERNEL_ELF=./build/kernel8.elf
 
 echo "Run OS on $BOARD"
 if [ "$BOARD" = "virt" ]; then
-    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2 -cpu cortex-a53 -smp 4 -device loader,file=$KERNEL_IMG,addr=0x40100000,cpu-num=0 -nographic -serial mon:stdio -net none -s $*
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2 -cpu cortex-a53 -smp 4 -kernel $KERNEL_ELF -nographic -serial mon:stdio -net none -s $*
 else
-    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=$KERNEL_IMG,addr=0x0 -nographic -serial null -serial mon:stdio -s $*
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel $KERNEL_ELF -nographic -serial null -serial mon:stdio -s $*
 fi
