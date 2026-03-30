8b580458bd641037fd427f60e67e0f2aa1dde8dc
Sho SHIMOMURA <graueule@gmail.com>
2026-03-30 21:34:47 +0900

    update runner script

--- run_os.sh
+++ run_os.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# -kernel オプションを使うと el2 から開始できるが、
-# イメージをロードするアドレスを指定できないのでフラットバイナリが使えない
-#DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel kernel8.img -nographic -serial null -serial mon:stdio -s $*
+BOARD=${BOARD:-raspi3}
+KERNEL_IMG=./os/raspios/build/kernel8.img
+KERNEL_ELF=./os/raspios/build/kernel8.elf
 
-DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=./os/raspios/build/kernel8.img,addr=0x0 -nographic -serial null -serial mon:stdio -s $*
+echo "Run OS on $BOARD"
+if [ "$BOARD" = "virt" ]; then
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2 -cpu cortex-a53 -smp 4 -kernel $KERNEL_ELF -nographic -serial mon:stdio -net none -s $*
+else
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel $KERNEL_ELF -nographic -serial null -serial mon:stdio -s $*
+fi
