0f2e61a7a80adf94bdda3c5d1672da65bdd39443
Sho SHIMOMURA <graueule@gmail.com>
2026-04-07 23:59:43 +0900

    fix: update debugger paths and add port mapping in run_command.sh

--- run_command.sh
+++ run_command.sh
@@ -3,4 +3,4 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 RELATIVE_PATH=$(pwd | sed "s|^${PROJECT_ROOT}||")
 
-docker run --rm -it -v ${PROJECT_ROOT}:/work -w /work${RELATIVE_PATH} -e BOARD=$BOARD raspvisor $*
+docker run --rm -it -v ${PROJECT_ROOT}:/work -w /work${RELATIVE_PATH} -p 1234:1234 -e BOARD=$BOARD raspvisor $*
