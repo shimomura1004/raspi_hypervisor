b9cbe5f56ab4d4d2d20bb5d696cf2a4ed9c2d69e
Sho SHIMOMURA <graueule@gmail.com>
2026-04-19 20:18:18 +0900

    skip port mapping if port 1234 is already in use

--- run_command.sh
+++ run_command.sh
@@ -3,4 +3,12 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 RELATIVE_PATH=$(pwd | sed "s|^${PROJECT_ROOT}||")
 
-docker run --rm -it -v ${PROJECT_ROOT}:/work -w /work${RELATIVE_PATH} -p 1234:1234 -e BOARD=$BOARD raspvisor $*
+# 1234 ポートが空いている場合のみマッピングを行う
+PORT_MAPPING=""
+if ! lsof -Pi :1234 -sTCP:LISTEN -t >/dev/null 2>&1; then
+    PORT_MAPPING="-p 1234:1234"
+else
+    echo "Port 1234 is in use, skipping GDB mapping."
+fi
+
+docker run --rm -it -v ${PROJECT_ROOT}:/work -w /work${RELATIVE_PATH} ${PORT_MAPPING} -e BOARD=$BOARD raspvisor $*
