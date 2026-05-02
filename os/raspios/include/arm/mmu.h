ada608bcd02765f2c2f4ead66b092bbda39de7ca
Sho SHIMOMURA <graueule@gmail.com>
2026-05-03 01:27:18 +0900

    refactor: raspios mmu.h

--- os/raspios/include/arm/mmu.h
+++ os/raspios/include/arm/mmu.h
@@ -3,33 +3,29 @@
 
 #include "arm/mmu_def.h"
 
-// todo: これも共通のはずだが名前が統一されていない
-#define MM_ACCESS_PERMISSION  (0x01 << 6) 
+// Memory region attributes:
+// HV と同じ設定になっているので詳細は hv/include/arm/mmu.h を参照
+//   n = AttrIndx[2:0]
+//                    n MAIR
+//   DEVICE_nGnRnE  000 00000000
+//   NORMAL_NC      001 01000100
+#define MT_DEVICE_nGnRnE        0x0
+#define MT_NORMAL_NC            0x1
+#define MT_DEVICE_nGnRnE_FLAGS  0x00
+#define MT_NORMAL_NC_FLAGS      0x44
+#define MAIR_VALUE \
+    ((MT_DEVICE_nGnRnE_FLAGS << (8 * MT_DEVICE_nGnRnE)) | \
+     (MT_NORMAL_NC_FLAGS << (8 * MT_NORMAL_NC)))
 
-/*
- * Memory region attributes:
- *
- *   n = AttrIndx[2:0]
- *			n	MAIR
- *   DEVICE_nGnRnE	000	00000000
- *   NORMAL_NC		001	01000100
- */
-#define MT_DEVICE_nGnRnE 		0x0
-#define MT_NORMAL_NC			0x1
-#define MT_DEVICE_nGnRnE_FLAGS		0x00
-#define MT_NORMAL_NC_FLAGS  		0x44
-#define MAIR_VALUE			(MT_DEVICE_nGnRnE_FLAGS << (8 * MT_DEVICE_nGnRnE)) | (MT_NORMAL_NC_FLAGS << (8 * MT_NORMAL_NC))
+#define MMU_FLAGS \
+    (MM_TYPE_BLOCK | (MT_NORMAL_NC << 2) | MM_S1_AF_ACCESS)
+#define MMU_DEVICE_FLAGS \
+    (MM_TYPE_BLOCK | (MT_DEVICE_nGnRnE << 2) | MM_S1_AF_ACCESS)
+#define MMU_PTE_FLAGS \
+    (MM_TYPE_PAGE | (MT_NORMAL_NC << 2) | MM_S1_AF_ACCESS | MM_S1_AP_RW_ALL)
 
-#define MMU_FLAGS	 		(MM_TYPE_BLOCK | (MT_NORMAL_NC << 2) | MM_S1_AF_ACCESS)	
-#define MMU_DEVICE_FLAGS		(MM_TYPE_BLOCK | (MT_DEVICE_nGnRnE << 2) | MM_S1_AF_ACCESS)	
-#define MMU_PTE_FLAGS			(MM_TYPE_PAGE | (MT_NORMAL_NC << 2) | MM_S1_AF_ACCESS | MM_ACCESS_PERMISSION)	
-
-#define TCR_T0SZ			(64 - 48) 
-#define TCR_T1SZ			((64 - 48) << 16)
-#define TCR_TG0_4K			(0 << 14)
-#define TCR_TG1_4K			(2 << 30)
-
-// todo: value だけ残るのが正しい
-#define TCR_VALUE			(TCR_T0SZ | TCR_T1SZ | TCR_TG0_4K | TCR_TG1_4K)
+// TCR_EL1 に設定する値(上位・下位アドレス空間とも、48 ビットアドレスかつページサイズは 4KB)
+#define TCR_VALUE \
+    (TCR_T0SZ(64 - 48) | TCR_T1SZ(64 - 48) | TCR_TG0_4K | TCR_TG1_4K)
 
 #endif
