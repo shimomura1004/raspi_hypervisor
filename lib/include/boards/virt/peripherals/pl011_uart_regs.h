0b0a172fdf9bbdd6a474b8cc431397620e2d9539
Sho SHIMOMURA <graueule@gmail.com>
2026-05-16 09:10:02 +0900

    update pl011 headers

--- lib/include/boards/virt/peripherals/pl011_uart_regs.h
+++ lib/include/boards/virt/peripherals/pl011_uart_regs.h
@@ -1,21 +1,28 @@
-#ifndef _P_PL011_UART_H
-#define _P_PL011_UART_H
-
-#include "peripherals/base.h"
+#ifndef _P_PL011_UART_REGS_H
+#define _P_PL011_UART_REGS_H
 
 // qemu virt board のメモリマップは qemu のソースコードで定義されている
 // https://github.com/qemu/qemu/blob/master/hw/arm/virt.c
 // [VIRT_UART0] = { 0x09000000, 0x00001000 }
-#define UART_BASE   (0x09000000)
 
-#define UART_DR     (UART_BASE + 0x00)
-#define UART_FR     (UART_BASE + 0x18)
-#define UART_IBRD   (UART_BASE + 0x24)
-#define UART_FBRD   (UART_BASE + 0x28)
-#define UART_LCRH   (UART_BASE + 0x2c)
-#define UART_CR     (UART_BASE + 0x30)
-#define UART_IMSC   (UART_BASE + 0x38)
-#define UART_ICR    (UART_BASE + 0x44)
+#define UART_DR_OFFSET      (0x00)
+#define UART_FR_OFFSET      (0x18)
+#define UART_IBRD_OFFSET    (0x24)
+#define UART_FBRD_OFFSET    (0x28)
+#define UART_LCRH_OFFSET    (0x2c)
+#define UART_CR_OFFSET      (0x30)
+#define UART_IMSC_OFFSET    (0x38)
+#define UART_ICR_OFFSET     (0x44)
+
+#define UART_BASE           (0x09000000)
+#define UART_DR             (UART_BASE + UART_DR_OFFSET)
+#define UART_FR             (UART_BASE + UART_FR_OFFSET)
+#define UART_IBRD           (UART_BASE + UART_IBRD_OFFSET)
+#define UART_FBRD           (UART_BASE + UART_FBRD_OFFSET)
+#define UART_LCRH           (UART_BASE + UART_LCRH_OFFSET)
+#define UART_CR             (UART_BASE + UART_CR_OFFSET)
+#define UART_IMSC           (UART_BASE + UART_IMSC_OFFSET)
+#define UART_ICR            (UART_BASE + UART_ICR_OFFSET)
 
 // UARTDR: Data register
 //   https://developer.arm.com/documentation/ddi0183/f/programmer-s-model/register-descriptions/data-register--uartdr
@@ -169,7 +176,4 @@
 #define UART_ICR_RXIC  (1 << 4) /* Receive interrupt clear */
 #define UART_ICR_RTIC  (1 << 6) /* Receive timeout interrupt clear */
 
-void uart_init(void);
-void handle_uart_irq(void);
-
-#endif  /*_P_PL011_UART_H */
+#endif  /*_P_PL011_UART_REGS_H */
