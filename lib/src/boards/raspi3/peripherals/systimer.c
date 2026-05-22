7815ce0828146367f466deb1c63396e469f08509
Sho SHIMOMURA <graueule@gmail.com>
2026-05-23 00:37:41 +0900

    refactor: rename variables

--- lib/src/boards/raspi3/peripherals/systimer.c
+++ lib/src/boards/raspi3/peripherals/systimer.c
@@ -5,16 +5,16 @@
 // RPi3 には 1tick ごとにカウントアップするタイマが搭載されていて
 // 合計4個の比較用レジスタがあり、カウンタの値が一致すると対応する割込み線を発火させる
 
-static unsigned long base_address;
+static unsigned long base_qa7_address;
 static unsigned int interval;
 
 void systimer_init(unsigned long base_virt_address, unsigned long systimer_interval) {
-    base_address = base_virt_address;
+    base_qa7_address = base_virt_address;
     interval = systimer_interval;
 
     // 今のカウンタ値から interval tick 後に発火するように2個目のレジスタに値をセットする
-    unsigned int next = get32(base_address + SYSTIMER_CLO_OFFSET) + interval;
-    put32(base_address + SYSTIMER_C1_OFFSET, next);
+    unsigned int next = get32(base_qa7_address + SYSTIMER_CLO_OFFSET) + interval;
+    put32(base_qa7_address + SYSTIMER_C1_OFFSET, next);
 }
 
 // システムタイマのレジスタ CLO/CHI を読み、合わせて64ビット値として返す
@@ -22,11 +22,11 @@ void systimer_init(unsigned long base_virt_address, unsigned long systimer_inter
 // 上位32ビットを2回読んで、更新されていたら読み直す
 // この対応がないと最大 2^32 us(約72分)ずれる
 unsigned long get_physical_systimer_count() {
-    unsigned long chi = get32(base_address + SYSTIMER_CHI_OFFSET);
-    unsigned long clo = get32(base_address + SYSTIMER_CLO_OFFSET);
-    if (chi != get32(base_address + SYSTIMER_CHI_OFFSET)) {
-        chi = get32(base_address + SYSTIMER_CHI_OFFSET);
-        clo = get32(base_address + SYSTIMER_CLO_OFFSET);
+    unsigned long chi = get32(base_qa7_address + SYSTIMER_CHI_OFFSET);
+    unsigned long clo = get32(base_qa7_address + SYSTIMER_CLO_OFFSET);
+    if (chi != get32(base_qa7_address + SYSTIMER_CHI_OFFSET)) {
+        chi = get32(base_qa7_address + SYSTIMER_CHI_OFFSET);
+        clo = get32(base_qa7_address + SYSTIMER_CLO_OFFSET);
     }
     return clo | (chi << 32);
 }
--- lib/src/boards/virt/gic.c
+++ lib/src/boards/virt/gic.c
@@ -1,55 +1,51 @@
-#include <inttypes.h>
-#include "utils.h"
+// implements lib/include/irq.h
 #include "board_config.h"
-#include "generic_timer.h"
-#include "mm.h"
+#include "utils.h"
 
-static unsigned long base_address;
-
-// todo: まだ init を呼んでない
-void init_interrupt_controller(unsigned long base_virt_address) {
-    base_address = base_virt_address;
-}
+// これらのオフセットは virt 専用
+// ベースアドレスは virt 向けに決め打ちなので、OS から仮想アドレスを渡してもらう必要はない
+static unsigned long base_gicc_address = GIC_CPU_BASE_OFFSET;
+static unsigned long base_gicd_address = GIC_DIST_BASE_OFFSET;
 
 // 各 CPU で実行する
 void enable_interrupt_controller(unsigned long cpuid) {
     // まず GIC の CPU interface を初期化
 
     // 全ての割込みを許可(0xff より小さい割込みをすべて許可)
-    put32(base_address + GICC_PMR_OFFSET, 0xff);
+    put32(base_gicc_address + GICC_PMR_OFFSET, 0xff);
     // CPU interface を有効化
-    put32(base_address + GICC_CTLR_OFFSET, GICC_CTLR_ENABLE);
+    put32(base_gicc_address + GICC_CTLR_OFFSET, GICC_CTLR_ENABLE);
 
     // 次に GIC の Distributor を初期化
     // 有効化するのは別途 enable_legacy_interrupt_controller() で行う
 
     // GICD_ISENABLER は Set-Enable レジスタなので、有効化したいビットだけを 1 にして書き込めばよい
     //   Set-Enable: 1 を書き込むと有効化され、0 を書き込むと変化しない(もとの値が保たれる)
-    put32(base_address + GICD_ISENABLER_OFFSET, (1 << IRQ_HYP_PHYS_TIMER) | (1 << IRQ_VIRT_TIMER));
+    put32(base_gicd_address + GICD_ISENABLER_OFFSET, (1 << IRQ_HYP_PHYS_TIMER) | (1 << IRQ_VIRT_TIMER));
 }
 
 // システムで1回実行する
 void enable_legacy_interrupt_controller(void) {
     // GIC Distributor を有効化
-    put32(base_address + GICD_CTLR_OFFSET, GICD_CTLR_ENABLE);
+    put32(base_gicd_address + GICD_CTLR_OFFSET, GICD_CTLR_ENABLE);
 
     // todo: GICD_ITARGETSR は GICv2 のためのもの、GICv3 以降では別の仕組みを使う
     // UART 割込みのターゲットを CPU 0 に設定
     // SPI 1 (ID 33) は (33 = 8x4+1) なので、GICD_ITARGETSR[8]  の 2 バイト目 (bits 8-15) に対応
-    unsigned int target_reg = get32(base_address + GICD_ITARGETSR_OFFSET + 32);
+    unsigned int target_reg = get32(base_gicd_address + GICD_ITARGETSR_OFFSET + 32);
     target_reg |= (0x01 << 8);
-    put32(base_address + GICD_ITARGETSR_OFFSET + 32, target_reg);
+    put32(base_gicd_address + GICD_ITARGETSR_OFFSET + 32, target_reg);
 
     // UART 割込みを有効化
     // SPI 1 の割込み番号は IRQ_UART0(33) で、ISENABLER[1] に相当する
-    put32(base_address + GICD_ISENABLER_OFFSET + 4, (1 << (IRQ_UART0 - 32)));
+    put32(base_gicd_address + GICD_ISENABLER_OFFSET + 4, (1 << (IRQ_UART0 - 32)));
 }
 
 // todo: これはドライバの API として用意する
 void enable_virtual_timer_irq(void) {
-    put32(base_address + GICD_ISENABLER_OFFSET, (1 << IRQ_VIRT_TIMER));
+    put32(base_gicd_address + GICD_ISENABLER_OFFSET, (1 << IRQ_VIRT_TIMER));
 }
 
 void disable_virtual_timer_irq(void) {
-    put32(base_address + GICD_ICENABLER_OFFSET, (1 << IRQ_VIRT_TIMER));
+    put32(base_gicd_address + GICD_ICENABLER_OFFSET, (1 << IRQ_VIRT_TIMER));
 }
