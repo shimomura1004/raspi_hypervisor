e74677c991ac12710445fa6d1868e73260827758
Sho SHIMOMURA <graueule@gmail.com>
2026-05-30 23:00:00 +0900

    bugfix: change EL to 1 when running as native OS

---
+++ os/echo/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# virt ボードの場合、-kernel オプションで ELF ファイルを指定すると
+# QEMU が ELF ファイルに埋め込まれた p_paddr を見てその場所にイメージをロードする
+# またそのとき PC の初期値は p_paddr に設定される
+# virtualization=on とすれば EL2 で、 virtualization=off とすれば EL1 で開始できる
+
+BOARD=${BOARD:-raspi3}
+KERNEL_IMG=./build/kernel8.img
+KERNEL_ELF=./build/kernel8.elf
+
+echo "Run OS on $BOARD"
+if [ "$BOARD" = "virt" ]; then
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2 -cpu cortex-a53 -smp 4 -kernel $KERNEL_ELF -nographic -serial mon:stdio -net none -s $*
+else
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel $KERNEL_ELF -nographic -serial null -serial mon:stdio -s $*
+fi
