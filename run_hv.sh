819649e5591a17592f94995ec3c3c560dafb9f72
Sho SHIMOMURA <graueule@gmail.com>
2026-06-14 18:13:04 +0900

    start qemu from EL3

--- run_hv.sh
+++ run_hv.sh
@@ -20,7 +20,7 @@ KERNEL_ELF=./hv/build/kernel8.elf
 
 echo "Run Hypervisor on $BOARD"
 if [ "$BOARD" = "virt" ]; then
-    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2,virtualization=on -cpu cortex-a53 -smp 4 \
+    DISPLAY=:0 qemu-system-aarch64 -m 1024 -M virt,gic-version=2,virtualization=on,secure=on -cpu cortex-a53 -smp 4 \
         -kernel $KERNEL_ELF \
         -nographic -serial mon:stdio -net none \
         -drive file=./hv/build/fs.img,if=none,format=raw,id=hd0 \
